5099e10cde53b789f7ba18cd72f18764f3d7b6a3
heechul <khc4190@naver.com>
2024-08-06 22:27:17 +0900

    회원 서비스 개발

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,17 +1,17 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
@@ -26,7 +26,7 @@ public class MemberRepository {
                 .getResultList();
     }
 
-    public List<Member> findByName(String name){
+    public List<Member> findByName(String name) {
         return em.createQuery("select m from Member m where m.name = :name", Member.class)
                 .setParameter("name", name)
                 .getResultList();
